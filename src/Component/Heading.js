3d145279f55f14d1ceb90a6a431d878ed442d963
Harshal Bharambe <hsb9604065774@gmail.com>
2024-02-16 00:32:09 +0530

    added

---
+++ src/Component/Heading.js
@@ -0,0 +1,26 @@
+import "./Heading.css";
+import IISC from './IISC.jpg'; 
+import NewLogo from './NewLogo.jpg';
+import HeadingNew from './HeadingNew.jpg';
+
+function Heading(){
+ return(
+    <>
+    <div class="heading">
+     <h3> Infinity Tech College of Computer Engineering (ITSC)</h3>
+    <h4> Accrecited by NAAC with A Grade in 2023 </h4>
+    <h5>Established in 1994</h5> 
+    </div>
+     <div>
+    <img class="chennailogo" src={IISC} alt="logo" width="200" />
+    </div>
+
+    <div>
+    <img class="cdaclogo" src={NewLogo} alt="logo" width="200" />
+    </div>
+    
+    </>
+ )
+}
+
+export default Heading;
